e852bdead541b12433c9a8510d2351b96f970407
wezur <s223838@student.uek.krakow.pl>
2022-03-20 19:38:59 +0100

    Added empty-value verification and multiple pages support

--- review.py
+++ review.py
@@ -2,13 +2,13 @@ class Review:
 	def __init__(self, reviewHtml):
 		self.reviewId = reviewHtml['data-entry-id']
 		self.author = reviewHtml.select('span.user-post__author-name')[0].text
-		self.recommendation = reviewHtml.select('span.user-post__author-recomendation > em')[0].text
-		self.starCount = reviewHtml.select('span.user-post__score-count')[0].text
-		self.reviewDate = reviewHtml.select('span.user-post__published > time[datetime]:nth-child(1)')[0]['datetime']
-		self.purchaseDate = reviewHtml.select('span.user-post__published > time[datetime]:nth-child(2)')[0]['datetime']
+		self.recommendation = reviewHtml.select('span.user-post__author-recomendation > em')[0].text if len(reviewHtml.select('span.user-post__author-recomendation > em')) > 0 else None
+		self.starCount = reviewHtml.select('span.user-post__score-count')[0].text if len(reviewHtml.select('span.user-post__score-count')) > 0 else None
+		self.reviewDate = reviewHtml.select('span.user-post__published > time[datetime]:nth-child(1)')[0]['datetime'] if len(reviewHtml.select('span.user-post__published > time[datetime]:nth-child(1)')) > 0 else None
+		self.purchaseDate = reviewHtml.select('span.user-post__published > time[datetime]:nth-child(2)')[0]['datetime'] if len(reviewHtml.select('span.user-post__published > time[datetime]:nth-child(2)')) > 0 else None
 		self.voteYesCount = reviewHtml.select('span[id^=votes-yes]')[0].text
 		self.voteNoCount = reviewHtml.select('span[id^=votes-no]')[0].text
-		self.content = reviewHtml.select('div.user-post__text')[0].text	
+		self.content = reviewHtml.select('div.user-post__text')[0].text	if len(reviewHtml.select('div.user-post__text')) > 0 else None
 		pros = reviewHtml.select('div.review-feature__col:has( > div.review-feature__title--positives)')
 		cons = reviewHtml.select('div.review-feature__col:has( > div.review-feature__title--negatives)')
 		self.pros = []
--- scraper.py
+++ scraper.py
@@ -2,24 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from review import Review
 
-request = requests.get('https://www.ceneo.pl/96092975#tab=reviews')
+hasNextPage = True
+pageNumber = 1
 
-soup = BeautifulSoup(request.text, 'html.parser')
+reviewList = []
+while(hasNextPage):
+	request = requests.get(f'https://www.ceneo.pl/96092975/opinie-{pageNumber}')
+	soup = BeautifulSoup(request.text, 'html.parser')
 
-test = Review(soup.select('div.js_product-reviews')[0].select('div.js_product-review')[0])
+	reviewListHtml = soup.select('div.js_product-reviews')[0].select('div.js_product-review')
+	for reviewEle in reviewListHtml:
+		reviewList.append(Review(reviewEle))
+	if len(soup.select('a.pagination__next')) > 0:
+		pageNumber += 1
+	else:
+		hasNextPage = False
 
-print(test.pros)
-# lista opinii - div.js_product-reviews
-# opinia - div.js_product-review
-# identyfikator opinii - div.js_product-review[data-entry-id]
-# autor - span.user-post__author-name
-# rekomendacja - span.user-post__author-recomendation > em
-# liczba gwiazdek - span.user-post__score-count
-# czy opinia jest potwierdzona zakupem
-# data wystawienia opinii - user-post__published > time[datetime]:nth-child(1)
-# data zakupu produktu - user-post__published > time[datetime]:nth-child(1)
-# ile osób uznało opinię za przydatną 
-# ile osób uznało opinię za nieprzydatną
-# treść opinii
-# lista wad
-# lista zalet
+print(reviewList)
